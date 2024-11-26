6ddeddc373d55d3902c6d084be86db6ab5fc5860
IgorLago <43051968+igorlago@users.noreply.github.com>
2024-11-26 17:14:34 -0300

    Add files via upload

---
+++ src/main/java/com/example/users/models/Task.java
@@ -0,0 +1,16 @@
+package com.example.users.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+}
+
+
